875b997a9945deed4f3d3b1e045ccaf981500e89
crissegura <103970933+crissegura@users.noreply.github.com>
2022-07-12 15:55:34 -0300

    Add files via upload

--- js/archivo.js
+++ js/archivo.js
@@ -1,8 +1,77 @@
-//let cliente = prompt('Ingresá tu nombre y apellido.')
+let enviar = document.getElementById('enviar')
+enviar.addEventListener('click',recogerDatos)
 
-//alert('Bienvenido, '+cliente+'.')
+function recogerDatos(){
 
-//alert('Precios: corte de pelo $800, barba $400, tintura $600.')
+    let nombreCliente = document.getElementById('nombre');
+
+    let corte = '';
+
+    let corteAdulto=document.getElementById('corteAdulto');
+    let corteNino=document.getElementById('corteNiño');
+    let pelarCero=document.getElementById('pelarCero');
+    let corteYBarba=document.getElementById('corteYBarba');
+    let ninguno=document.getElementById('ninguno');
+    if(corteAdulto.checked){
+        corte = corteAdulto.value
+    }else if (corteNino.checked){
+        corte = corteNino.value
+    }else if (pelarCero.checked){
+        corte = pelarCero.value
+    }else if (corteYBarba.checked){
+        corte = corteYBarba.value
+    }else if(ninguno.checked){
+        corte = ninguno.value
+    }
+
+    let barba = '';
+
+    let completa=document.getElementById('completa');
+    let rebajar=document.getElementById('soloRebaje');
+    let afeitar=document.getElementById('afeitar');
+    let ninguno2=document.getElementById('ninguno2');
+    if(completa.checked){
+        barba = completa.value
+    }else if (rebajar.checked){
+        barba = rebajar.value
+    }else if (afeitar.checked){
+        barba = afeitar.value
+    }else if(ninguno2.checked){
+        barba = ninguno2.value
+    }
+
+    let color = '';
+
+    let tintura=document.getElementById('tintura');
+    let reflejos=document.getElementById('reflejos');
+    let platinado=document.getElementById('platinado');
+    let ninguno3=document.getElementById('ninguno3');
+    if(tintura.checked){
+        color = tintura.value
+    }else if (reflejos.checked){
+        color = reflejos.value
+    }else if (platinado.checked){
+        color = platinado.value
+    }else if(ninguno3.checked){
+        color = ninguno3.value
+    }
+
+    let turno = nombreCliente.value+', seleccionaste los servicios: corte de cabello: '+corte+', barba: '+barba+', tintura '+color+'. Esperamos tu visita!'
+
+    alert(turno)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
